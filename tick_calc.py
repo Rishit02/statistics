db30ce28c518f6edfeec821844a136f1091b6d85
Rishit Khandelwal <rishit@Admins-iMac.local>
2020-09-19 14:28:56 +0800

    made a loop to to read files in a directory

--- tick_calc.py
+++ tick_calc.py
@@ -5,5 +5,10 @@ import pandas
 def main():
   pass
 
+def load_data(directory="files"):
+    for file in os.listdir(directory):
+        pd.read_csv(f"file")
+
+
 if __name__ == "__main__":
     main()
