c4a105e124307c73cb66502e174298234649737c
Rishit Khandelwal <rishit@MacBook-Air.local>
2020-09-26 22:54:45 +0800

    have included ability to filter out the data s that only specific future month_codes are included

--- tick_calc.py
+++ tick_calc.py
@@ -10,11 +10,20 @@ from datetime import datetime, timedelta
 def main():
     timeframe = str(input("What's the timeframe? {week, month or year}: ")).lower()
     print("Loading data...")
-    df = load_data()
-    # path = split_minute(dataframe=df)
-    # df = pd.read_csv(f"{path}")
+    df = pd.read_csv("/Volumes/TICK/spreadsheets/all_files.csv")
+    df = df[0:5000]
+    df['Time'] = pd.to_datetime(df['Time'], format="%Y-%m-%d %H:%M:%S")
+
+    print("Splitting into the initial minutes")
+    path = split_minute(dataframe=df)
+    print("Reading into the minutes.csv file")
+    df = pd.read_csv(f"{path}")
+    df['Time'] = pd.to_datetime(df['Time'], format="%Y-%m-%d %H:%M:%S")
 
     print("Splitting data...")
+    # data = split(dataframe=df)  # Yearly
+    # data = split_to_monthly(dataframe=df)   # Monthly
+    # data = split_to_weekly(dataframe=df)    # Weekly
     if timeframe[0] == 'y':
         data = split(dataframe=df)
         if timeframe == 'y':
@@ -31,89 +40,86 @@ def main():
     the_loop(matrix=data, timeframe=timeframe)
 
 """
-Loading the data into a dataframe
-"""
-def load_data(directory="/Volumes/TICK/required_info"):
-    month_codes = {"Jan":"F", "Feb":"G", "Mar":"H", "Apr":"J", "May":"K", "Jun":"M", "Jul":"N", "Aug":"Q", "Sep":":U", "Oct":"V", "Nov":"X", "Dec":"Z"}
-    # Collect all the csv files into one DataFrame
-    all_files = list()
-    dir_list = sorted(os.listdir(directory))
-    for file in dir_list:
-        file = str(file)
-        if file[0:2] == "._":
-            continue
-            print('._', file)
-        print(file)
-        csv_file = pd.read_csv(f"/Volumes/TICK/required_info/{file}")
-        csv_file['Time'] = csv_file[csv_file.columns[5:7]].apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
-        csv_file['Time'] = pd.to_datetime(csv_file['Time'], format="%Y%m%d,%H%M%S")
-        del df['Log_Time']
-        del df['Year']
-        csv_file = csv_file.loc[(csv_file['Msg_Code'] == "T")]
-        all_files.append(csv_file)
-
-    all_files = pd.concat(all_files, axis=0, ignore_index=True)
-
-    print(f"All files header\n{all_files.head(5)}")
-    all_files.to_csv("all_files.csv")
-    return pd.DataFrame(all_files)
-
-"""
 Splitting the data into yearly, weekly and monthly values
 """
 def split_minute(dataframe):
     mins = [g for n, g in dataframe.groupby(pd.Grouper(key='Time',freq='Min'))]
     minutes = list()
-
+    month_codes = {"F":1, "G":2, "H":3, "J":4, "K":5, "M":6, "N":7, "Q":8, "U":9, "V":10, "X":11, "Z":12}
     for i in range(len(mins)):
-        min = pd.DataFrame(mins[i])
-        print("Min: \n", min.head(3))
-        if min.empty:
+        min_data = pd.DataFrame(mins[i])
+        print("min_data: \n", min_data.head(3))
+        if min_data.empty:
             print("EMPTY:", i)
-            del min
+            del min_data
         else:
-            min.reset_index(drop=True, inplace=True)
-            time = min.at[0, 'Time']
-            # df.insert(0, 'Time', (min.at[0, 'Time']))
-            # df.insert(1, 'Open', int(min.at[0, 'Price']))
-            min['Time'] = time
+            # Reset the index
+            min_data.reset_index(drop=True, inplace=True)
+            mth_num = np.array([])
+            sr = pd.Series(min_data["Mth_Code"])
 
-            min['Open'] = min['Price'][0]
-            min['Close'] = min['Price'][len(min) - 1]
-            # df.insert(2, 'Close', int(min.at[len(min.index), 'Price']))
+            # To filter out the required month codes
+            for item in sr.iteritems():
+                mth_num = np.append(mth_num, month_codes[item[1]])
+
+            print("mth_num", mth_num)
+            min_data["Mth_num"] = mth_num
+            min_data['Months'] = pd.DatetimeIndex(min_data['Time']).month
+            min_data['diff'] = min_data["Mth_num"] - min_data['Months']
+            print("Futures market 3 mnths ahead")
+            print(min_data)
+            min_data.drop(min_data[min_data["diff"] != 3].index, inplace=True) # Change the number here in order to change the number of years ahead you want the furtures market
+
+            # If all the rows are deleted continue with the next min_data
+            if min_data.empty:
+                print("None:", i)
+                del min_data
+                continue
+
+            min_data.reset_index(drop=True, inplace=True)
+            time = min_data.at[0, 'Time']
+            min_data['Time'] = time
+
+            min_data['Open'] = min_data['Price'][0]
+            min_data['Close'] = min_data['Price'][len(min_data) - 1]
+            # df.insert(2, 'Close', int(min_data.at[len(min_data.index), 'Price']))
 
             # Sorting the values to get high low
-            print("min and i is \n", min, i)
-            # df.insert(3, 'High', min.at[len(min.index), 'Price'])
-            min.sort_values(by=['Price'], ascending=True, inplace=True)
-            min.reset_index(drop=True, inplace=True)
-            min['Low'] = min['Price'][0]
-            min['High'] = min['Price'][len(min)-1]
-            print("min.head() is: \n", min.head(5))
+            print("min_data and i is \n", min_data, i)
+            # df.insert(3, 'High', min_data.at[len(min_data.index), 'Price'])
+            min_data.sort_values(by=['Price'], ascending=True, inplace=True)
+            min_data.reset_index(drop=True, inplace=True)
+            min_data['Low'] = min_data['Price'][0]
+            min_data['High'] = min_data['Price'][len(min_data)-1]
+            print("min_data.head() is: \n", min_data.head(5))
 
-            print("min.head() is: \n", min.head(5))
-            print("MIN.columns is: \n", min.columns)
+            print("min_data.head() is: \n", min_data.head(5))
+            print("min_data.columns is: \n", min_data.columns)
 
-            minutes.append(min)
+            minutes.append(min_data)
 
-    print("minutes is: \n", minutes, type(minutes))
+    print("minutes is: \n", minutes[0:10], type(minutes))
 
-    minutes = pd.concat(minutes, axis=0, ignore_index=True)
-    minutes.to_csv("minutes.csv")
+    try:
+        minutes = pd.concat(minutes, axis=0, ignore_index=True)
+        minutes.to_csv("minutes.csv")
+    except Exception as e:
+        print(e)
+
     return f"minutes.csv"
 
 def split(dataframe):
     years = [g for n, g in dataframe.groupby(pd.Grouper(key='Time',freq='Y'))]
     for i in range(len(years)):
         year = pd.DataFrame(years[i])
-        year.to_csv(f"year_{i}.csv", columns=["Time", "Price"], index=False)
+        year.to_csv(f"year_{i}.csv", columns=["Time", "Close"], index=False)
     print(years)
     return years
 def split_to_weekly(dataframe):
     weeks = [g for n, g in dataframe.groupby(pd.Grouper(key='Time',freq='W'))]
     for i in range(len(weeks)):
         week = pd.DataFrame(weeks[i])
-        week.to_csv(f"week_{i}.csv", columns=["Time", "Price"], index=False)
+        week.to_csv(f"week_{i}.csv", columns=["Time", "Close"], index=False)
     print("Weeks", weeks)
     return weeks
 
@@ -121,7 +127,7 @@ def split_to_monthly(dataframe):
     months = [g for n, g in dataframe.groupby(pd.Grouper(key='time',freq='M'))]
     for i in range(len(months)):
         month = pd.DataFrame(months[i])
-        month.to_csv(f"month_{i}.csv", columns=["Time", "Price"], index=False)
+        month.to_csv(f"month_{i}.csv", columns=["Time", "Close"], index=False)
     print("Months", months)
     return months
 
@@ -137,8 +143,8 @@ def the_loop(matrix, timeframe):
     used_lag = list()
     times = list()
     for i in range(len(matrix)): # dataframe == weeks
-        time = str(matrix[i]["time"]).split()[1]
-        print("type", type(matrix[i]["time"]))
+        time = str(matrix[i]["Time"]).split()[1]
+        print("type", type(matrix[i]["Time"]))
         times.append(time)
         statistics = adf_test(f"{timeframe}_{i}.csv")
         print("stats", statistics)
@@ -146,7 +152,7 @@ def the_loop(matrix, timeframe):
         p_value.append(statistics[1])
         used_lag.append(statistics[2])
         os.remove(f"{timeframe}_{i}.csv")
-    data["time"] = times
+    data["Time"] = times
     data["ADF"] = ADF_stats
     data["p_value"] = p_value
     data["used_lag"] = used_lag
@@ -183,6 +189,34 @@ def adf_test(path):
 
     print(result)
     return result[0], result[1], result[2]
+"""
+Loading the data into a dataframe
+"""
+def load_data(directory="/Volumes/TICK/required_info"):
+    month_codes = {"F":1, "G":2, "H":3, "J":4, "K":5, "M":6, "N":7, "Q":8, "U":9, "V":10, "X":11, "Z":12}
+    # Collect all the csv files into one DataFrame
+    all_files = list()
+    dir_list = sorted(os.listdir(directory))
+    for file in dir_list:
+        file = str(file)
+        if file[0:2] == "._":
+            continue
+            print('._', file)
+        print(file)
+        csv_file = pd.read_csv(f"/Volumes/TICK/required_info/{file}")
+        csv_file['Time'] = csv_file[csv_file.columns[5:7]].apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
+        csv_file['Time'] = pd.to_datetime(csv_file['Time'], format="%Y%m%d,%H%M%S")
+        del csv_file['Log_Time']
+        del csv_file['Year']
+        csv_file = csv_file.loc[(csv_file['Msg_Code'] == "T")]
+        all_files.append(csv_file)
+
+    all_files = pd.concat(all_files, axis=0, ignore_index=True)
+
+    print(f"All files header\n{all_files.head(5)}")
+    all_files.to_csv("all_files.csv")
+    return pd.DataFrame(all_files)
+
 
 if __name__ == "__main__":
     main()
