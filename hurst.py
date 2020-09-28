b105ad9a17e70f54da130ecf113844fe7077c139
Rishit Khandelwal <rishit@MacBook-Air.local>
2020-09-28 15:29:42 +0800

    Have finished the calculation of hurst

--- hurst.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from hurst import compute_Hc, random_walk
-
-df = pd.read_csv("/Volumes/TICK/spreadsheets/all_files.csv")
-series = pd.Series(df["Price"])
