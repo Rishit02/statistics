b0de58d8ea36aceb52d02551108d38481077e61b
Rishit Khandelwal <rishit@MacBook-Air.local>
2020-09-28 21:27:26 +0800

    comment out the plotting code

--- hurst_calc.py
+++ hurst_calc.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from hurst import compute_Hc
 from pylab import plot, show
 
-
 def main():
     df = pd.read_csv("minutes.csv")
     df = df[0:400]
@@ -17,16 +16,18 @@ def main():
     H, c, data = compute_Hc(series, kind='price', simplified=True)
     print(f"H={H:.4f}, c={c:.4f}")
 
-    # Plot
-    f, ax = plt.subplots()
-    ax.plot(data[0], c*data[0]**H, color="deepskyblue")
-    ax.scatter(data[0], data[1], color="purple")
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_xlabel('Time interval')
-    ax.set_ylabel('R/S ratio')
-    ax.grid(True)
-    plt.show()
+    # Calculate for the daily data the last transaction in the day
+
+    # # Plot
+    # f, ax = plt.subplots()
+    # ax.plot(data[0], c*data[0]**H, color="deepskyblue")
+    # ax.scatter(data[0], data[1], color="purple")
+    # ax.set_xscale('log')
+    # ax.set_yscale('log')
+    # ax.set_xlabel('Time interval')
+    # ax.set_ylabel('R/S ratio')
+    # ax.grid(True)
+    # plt.show()
 
 if __name__ == "__main__":
     main()
